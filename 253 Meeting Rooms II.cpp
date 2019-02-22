8a058c4920f60951d74360a13286894d7b94bce2
minghuiJoyce <46167492+minghuiJoyce@users.noreply.github.com>
2019-02-22 13:15:15 -0500

    Update 253 Meeting Rooms II.cpp

--- 253 Meeting Rooms II.cpp
+++ 253 Meeting Rooms II.cpp
@@ -36,3 +36,44 @@ public:
     
     }
 };
+/*
+解法2：minHeap
+/**
+ * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
+ */
+class Solution {
+public:
+    static bool compareI(const Interval& a, const Interval& b){
+        return a.start<b.start;
+    }
+    
+    int minMeetingRooms(vector<Interval>& intervals) {        
+        //method 2: usig priority_queue: minHeap
+        //compare condition: sort intervals by starting time,
+        //see if the lastest end time is smaller than current starting time; if smaller, then a room can now be used for another interview
+        sort(intervals.begin(), intervals.end(), compareI);
+        priority_queue<int,vector<int>, greater<int> > queue;
+        int room = 0;
+        int sizeI = intervals.size();
+        for(int i = 0; i< sizeI;i++){
+            queue.push(intervals[i].end);
+        }
+        for(int i = 0;i<sizeI;i++){
+            if(intervals[i].start<queue.top()){
+                room++;
+            }
+            else{
+                queue.pop();
+            }
+        }
+        return room;
+        
+    }
+};
+*/

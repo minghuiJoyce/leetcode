be8a693ae35936b39c8a66cf719fb5eac7bf6599
minghuiJoyce <46167492+minghuiJoyce@users.noreply.github.com>
2019-02-22 13:15:43 -0500

    Update 253 Meeting Rooms II.cpp

--- 253 Meeting Rooms II.cpp
+++ 253 Meeting Rooms II.cpp
@@ -38,15 +38,7 @@ public:
 };
 /*
 解法2：minHeap
-/**
- * Definition for an interval.
- * struct Interval {
- *     int start;
- *     int end;
- *     Interval() : start(0), end(0) {}
- *     Interval(int s, int e) : start(s), end(e) {}
- * };
- */
+
 class Solution {
 public:
     static bool compareI(const Interval& a, const Interval& b){
